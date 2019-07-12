b4863fd616ef763a7ab1c1a32a0adc79be1092f5
Yancey Leo <yanceyofficial@gmail.com>
2019-07-12 15:19:45 +0800

    feat: Add UI framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.css';
 import Layouts from './layouts/Layouts';
 import * as serviceWorker from './serviceWorker';
--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
-  return <main className='layouts'>hello, rxjs</main>;
+  return (
+    <main className='layouts'>
+      <Button variant='contained' color='primary'>
+        Hello, RxJS!
+      </Button>
+    </main>
+  );
 };
 
 export default App;
