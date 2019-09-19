2f4de9e84ee0b9b8b8120ec5d737152562bed211
YanceyOffical <yanceyofficial@gmail.com>
2019-09-19 16:58:01 +0800

    feat: delete useless codes

--- src/containers/Hajimete.tsx
+++ src/containers/Hajimete.tsx
@@ -1,88 +1,72 @@
-import React from 'react';
-
-import { fromEvent, Observable, Subscriber } from 'rxjs';
-import { scan, throttleTime, map, take } from 'rxjs/operators';
-
-import Button from '@material-ui/core/Button';
+import React, { FC, useEffect } from 'react'
+import { fromEvent, Observable, Subscriber } from 'rxjs'
+import { scan, throttleTime, map, take } from 'rxjs/operators'
+import { Button } from 'semantic-ui-react'
 
 // RxJS is a library for composing asynchronous and event-based programs by using observable sequences.
 // RxJS是 一个 通过使用可观察序列 来组合 异步和基于事件的程序 的库.
-class Hajimete extends React.Component<{}, {}> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {};
-  }
-
-  public componentDidMount() {
-    this.firstRxJS();
-    this.onObservable();
-  }
-
-  public firstRxJS() {
-    const button = document.querySelector('.button');
-    fromEvent(button as any, 'click')
-      .pipe(
-        // 只能点击两次 (该函数只能被调用两次)
-        take(2),
-        // 任务管道化
-        // 先通过节流，在进行 count 的自增
-        throttleTime(1000),
-        // 在点击的同时获取 e.clientY
-        map(e => (e as any).clientY),
-        // 变量初始化不会暴在外部，保持纯净性
-        scan((count, clientX) => count + clientX, 0),
-      )
-      // 最后返回订阅结果
-      .subscribe(count => console.log(`我为长者+${count}s`));
-  }
-
-  public onObservable() {
-    let count = 0;
-    const observable = new Observable((subscriber: Subscriber<any>) => {
-      try {
-        subscriber.next(++count);
-        subscriber.next(++count);
-        const timer = setInterval(() => {
-          subscriber.next(++count);
-          // clearInterval(timer);
-          // subscriber.complete();
-        }, 1000);
-        return function unsubscribe() {
-          clearInterval(timer);
-        };
-      } catch (e) {
-        subscriber.error('Something wrong...');
-      }
-    });
-
-    console.log('just before subscribe'); // 1
-    const subscription = observable.subscribe({
-      next(x) {
-        // 2 同时打印出值 1 和 2
-        // 4 1s后打印出 3
-        console.log('got value ' + x);
-      },
-      error(err) {
-        console.error('something wrong occurred: ' + err);
-      },
-      complete() {
-        // 4 1s后打印
-        console.log('done');
-      },
-    });
-    console.log('just after subscribe'); // 3
-    subscription.unsubscribe();
-  }
-
-  public render() {
-    return (
-      <section className='layouts'>
-        <Button variant='contained' color='primary' className='button'>
-          Hello, RxJS!
-        </Button>
-      </section>
-    );
-  }
+const firstRxJS = () => {
+  const button = document.querySelector('.button')
+  fromEvent(button as any, 'click')
+    .pipe(
+      // 只能点击两次 (该函数只能被调用两次)
+      take(2),
+      // 任务管道化
+      // 先通过节流，在进行 count 的自增
+      throttleTime(1000),
+      // 在点击的同时获取 e.clientY
+      map(e => (e as any).clientY),
+      // 变量初始化不会暴在外部，保持纯净性
+      scan((count, clientX) => count + clientX, 0),
+    )
+    // 最后返回订阅结果
+    .subscribe(count => console.log(`我为长者+${count}s`))
 }
 
-export default Hajimete;
+const onObservable = () => {
+  let count = 0
+  const observable = new Observable((subscriber: Subscriber<any>) => {
+    try {
+      subscriber.next(++count)
+      subscriber.next(++count)
+      const timer = setInterval(() => {
+        subscriber.next(++count)
+        // clearInterval(timer);
+        // subscriber.complete();
+      }, 1000)
+      return function unsubscribe() {
+        clearInterval(timer)
+      }
+    } catch (e) {
+      subscriber.error('Something wrong...')
+    }
+  })
+
+  console.log('just before subscribe') // 1
+  const subscription = observable.subscribe({
+    next(x) {
+      // 2 同时打印出值 1 和 2
+      // 4 1s后打印出 3
+      console.log('got value ' + x)
+    },
+    error(err) {
+      console.error('something wrong occurred: ' + err)
+    },
+    complete() {
+      // 4 1s后打印
+      console.log('done')
+    },
+  })
+  console.log('just after subscribe') // 3
+  subscription.unsubscribe()
+}
+
+const Hajimete: FC = () => {
+  useEffect(() => {
+    firstRxJS()
+    onObservable()
+  }, [])
+  return <Button secondary>Hello, RxJS!</Button>
+}
+
+export default Hajimete
--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -2,23 +2,38 @@ import React, { FC } from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
 
+import Hajimete from 'containers/Hajimete'
 import CreationOperators from 'containers/CreationOperators'
 import CombinedOperators from 'containers/CombinedOperators'
 
 const Layouts: FC = () => {
   return (
-    <main className='layouts'>
-      <Button primary>
-        <Link to='/creationOperators'>creationOperators</Link>
-      </Button>
-      <Button primary>
-        <Link to='/combinedOperators'>combinedOperators</Link>
-      </Button>
-      <Switch>
-        <Route path='/creationOperators' render={() => <CreationOperators />} />
-        <Route path='/combinedOperators' render={() => <CombinedOperators />} />
-      </Switch>
-    </main>
+    <section className='layouts'>
+      <nav>
+        <Button primary>
+          <Link to='/Hajimete'>Hajimete</Link>
+        </Button>
+        <Button primary>
+          <Link to='/CreationOperators'>CreationOperators</Link>
+        </Button>
+        <Button primary>
+          <Link to='/CombinedOperators'>CombinedOperators</Link>
+        </Button>
+      </nav>
+      <main className='mainContent'>
+        <Switch>
+          <Route path='/Hajimete' render={() => <Hajimete />} />
+          <Route
+            path='/CreationOperators'
+            render={() => <CreationOperators />}
+          />
+          <Route
+            path='/CombinedOperators'
+            render={() => <CombinedOperators />}
+          />
+        </Switch>
+      </main>
+    </section>
   )
 }
 
